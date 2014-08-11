87ef9c71b88287588e6be8b861c5fdb91e30b1e9
damian <damian@damian.id.au>
2014-08-11 12:05:56 +1000

    testing

---
+++ damibawt.tcl
@@ -0,0 +1,48 @@
+bind pubm n *,* pub:cmd
+
+proc pub:cmd {nick uhost handle chan text} {
+  # <botnick>, <command> <args>
+  # bots, <command> <args>
+  global botnick
+  if {([string tolower [lindex $text 0]] == "bots,") || ([string tolower [lindex $text 0]] == "$botnick,")} {
+    switch [lindex $text 1] {
+      "rehash" {
+        if {[catch {rehash} output]} {
+          notice $nick "An error occurred while rehashing:"
+          foreach line [split $ouptput \r\n] {
+            notice $nick $line
+          }
+        } else {
+          notice $nick "Successfully rehashed."
+        }
+      }
+      "status" {
+      }
+      "load" {
+        if {[catch {rehash} output]} {
+          notice $nick "An error occurred while rehashing:"
+          foreach line [split $ouptput \r\n] {
+            notice $nick $line
+          }
+        } else {
+          notice $nick "Successfully rehashed."
+        }
+      }
+      "raw" {
+        putquick [lrange $text 2 end]
+        notice $nick "Put to server: [lrange $text 2 end]"
+      }
+      default {
+        catch {eval [lrange $text 1 end]} output
+        foreach line [split $output \r\n] {
+          notice $nick "Tcl: $line"
+        }
+        notice $nick "End."
+      }
+    }
+  }
+}
+
+proc notice {nick text} {
+  puthelp "NOTICE $nick :$text"
+}
