3a61936f66c690a73bda1edf7eb286e29373f609
Server Installations <server@devel.nictitate.net>
2014-08-11 16:32:52 +1000

    updated the status, added more ideas to the script

--- damibawt.tcl
+++ damibawt.tcl
@@ -17,21 +17,49 @@ proc pub:cmd {nick uhost handle chan text} {
         }
       }
       "status" {
-        notice $nick ".. a status?"
+        if {[catch {
+          # get ip info
+          set ifconfig [exec /sbin/ifconfig]
+
+          foreach line [split $ifconfig \r\n] {
+            set line [string trim $line]
+            if {[lindex $line 0] == "inet"} {
+              set ip([lindex [split [lindex $line 1] :] 1]) [lindex [split [lindex $line 1] :] 1]
+            }
+          }
+
+          notice $nick "IP Address: [array names ip]"
+          notice $nick "    Uptime: [exec uptime]"
+          notice $nick ""
+        } output]} {
+          notice $nick "had an error... $output"
+        }
       }
       "load" {
         if {[catch {rehash} output]} {
-          notice $nick "An error occurred while rehashing:"
+          notice $nick "An error occurred:"
           foreach line [split $ouptput \r\n] {
             notice $nick $line
           }
         } else {
-          notice $nick "Successfully rehashed."
+          notice $nick "Successfully loaded '[lrange $text 2 end]'"
         }
       }
       "raw" {
         putquick [lrange $text 2 end]
         notice $nick "Put to server: [lrange $text 2 end]"
+      }
+     "project" {
+       switch [string tolower [lindex $text 2]] {
+         "add" {
+           # project add <type> <folder> <description>
+         }
+         "list" {
+           # project list <wildcard>
+         }
+         "del" {
+           # project del <id>
+         }
       }
       default {
         catch {eval [lrange $text 1 end]} output
@@ -45,5 +73,5 @@ proc pub:cmd {nick uhost handle chan text} {
 }
 
 proc notice {nick text} {
-  puthelp "NOTICE $nick :$text"
+  putquick "NOTICE $nick :$text"
 }
